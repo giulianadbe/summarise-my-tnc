7d44540f4b3119a119dcf1fd5fb71d3216a68d95
edwinaadisusila <e.adisusila@gmail.com>
2021-08-28 19:29:38 +1000

    getting current website url and fetching footer

--- popup.js
+++ popup.js
@@ -1,20 +1,26 @@
-// grab the website we are currently looking at 
+chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    // get current website url 
+    let url = tabs[0].url;
+    var website = document.getElementById('website');
+    website.textContent = url;
 
-// fetch the html from the website
-fetch('https://www.theiconic.com.au').then(function (response) {
-    // The API call was successful!
-    return response.text();
-}).then(function (data) {
-    // search for the <footer> tag 
-    var parser = new DOMParser();
-    var doc = parser.parseFromString(data, 'text/html');
-    var footer = doc.querySelector('footer');
-    var box = document.getElementById('box');
-    // box.textContent = footer.textContent;
+    // fetch the html from the website
+    fetch(url).then(function (response) {
+        // The API call was successful!
+        return response.text();
+    }).then(function (data) {
+        // search for the <footer> tag 
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(data, 'text/html');
+        var footer = doc.querySelector('footer');
+        var box = document.getElementById('box');
+        box.textContent = footer.textContent;
 
-    // search for the <a> tag with the terms & conditions in footer
+        // search for the <a> tag with the terms & conditions in footer
 
-}).catch(function (err) {
-    // There was an error
-    console.warn('Something went wrong.', err);
+    }).catch(function (err) {
+        // There was an error
+        console.warn('Something went wrong.', err);
+    });
 });
+
