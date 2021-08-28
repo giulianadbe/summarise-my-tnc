52c1e00b65ebd2e0d29ecd3dd87fb414d6063544
Giuliana De Bellis <z5309331@unsw.edu.au>
2021-08-28 19:59:01 +1000

    Get terms and conditions link from footer

--- popup.js
+++ popup.js
@@ -10,9 +10,29 @@ fetch('https://www.theiconic.com.au').then(function (response) {
     var doc = parser.parseFromString(data, 'text/html');
     var footer = doc.querySelector('footer');
     var box = document.getElementById('box');
-    // box.textContent = footer.textContent;
 
     // search for the <a> tag with the terms & conditions in footer
+    // get href values in the footer
+    let array = [];
+    let links = footer.getElementsByTagName("a");
+    for (var i=0, max=links.length; i<max; i++) {
+        array.push(links[i].href);
+    }
+
+    // try match regex for selected keywords
+    let regexList = [/term/, /condition/, /legal/]; // do research here
+    let matches = [];
+    array.forEach((link) => {
+        matches = regexList.filter((word) => word.test(link));
+        if (matches.length === 0) {
+            console.log("No matches");
+        } else {
+            let content = link.split('/')[3];
+            // output match
+            box.textContent = 'EDDYS-WEB-LINK' + content;
+            return;
+        }
+    });
 
 }).catch(function (err) {
     // There was an error
